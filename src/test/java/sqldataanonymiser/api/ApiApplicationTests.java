fad0bb5dc17f56b9415af43416a5fe80193bbd2a
ragatzino <antoinebrunetti@gmail.com>
2020-05-22 17:54:55 +0200

    init and first time action

---
+++ src/test/java/sqldataanonymiser/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package sqldataanonymiser.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
